5870d4ab49eebf9189cb5431ec02dc03a5f3b94d
Alexander Popov <alex.popov@linux.com>
2020-04-14 01:54:40 +0300

    run: ask which container to run and remove it after exit

--- run.sh
+++ run.sh
@@ -3,8 +3,11 @@
 set -x
 set -e
 
-#GCC_VERSION=8
-GCC_VERSION=7
+if [ $# != 1 ];
+then
+	echo "usage: $0 gcc_version"
+	exit 1
+fi
 
 # Z for setting SELinux label
-sudo docker run -v /home/a13x/Develop_local/linux:/home/$(id -nu)/src:Z -it kernel-build-container:${GCC_VERSION}
+sudo docker run --rm -v /home/a13x/Develop_local/linux:/home/$(id -nu)/src:Z -it kernel-build-container:$1
