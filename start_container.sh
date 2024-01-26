7e58df15ee49d7de1a3288494e46a03fb147ac4c
Valentin Obst <valentin.obst@fkie.fraunhofer.de>
2024-01-26 10:45:57 +0100

    start_container: support setting env vars

--- start_container.sh
+++ start_container.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
+function print_help {
+	echo "usage: $0 compiler src_dir out_dir [-n] [-h] [-e] [-- cmd with args]"
+	echo "  -n    launch container in non-interactive mode"
+	echo "  -e    space-separated list of environment variables"
+	echo "  -h    print this help"
+	echo ""
+	echo "  If cmd is empty, we will start an interactive bash in the container."
+}
+
 groups | grep docker
 NEED_SUDO=$?
+
+set -eu
 
 if [ $NEED_SUDO -eq 1 ]
 then
@@ -12,13 +23,9 @@ else
 	SUDO_CMD=""
 fi
 
-set -e
-
 if [ $# -lt 3 ]
 then
-	echo "usage: $0 compiler src_dir out_dir [-n] [cmd with args]"
-	echo "  use '-n' for non-interactive session"
-	echo "  if cmd is empty, we will start an interactive bash in the container"
+	print_help
 	exit 1
 fi
 
@@ -31,19 +38,48 @@ echo "Source code directory \"$SRC\" is mounted at \"~/src\""
 OUT="$3"
 echo "Build output directory \"$OUT\" is mounted at \"~/out\""
 
-shift
-shift
-shift
+shift 3
 
-if [ $# -gt 0 -a "$1" = "-n" ]
+# defaults
+CIDFILE=""
+ENV=""
+INTERACTIVE="-it"
+
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -n|--non-interactive)
+      INTERACTIVE=""
+      CIDFILE="--cidfile $OUT/container.id"
+      echo "Run docker in NON-interactive mode"
+      shift
+      ;;
+    -e|--env)
+      for var in $2
+      do
+        ENV="$ENV -e $var"
+      done
+      shift 2
+      ;;
+    -h|--help)
+      print_help
+      exit 0
+      ;;
+    --)
+      shift
+      break
+      ;;
+    -*|--*)
+      echo "Unknown option $1"
+      print_help
+      exit 1
+      ;;
+  esac
+done
+
+echo "Container environment: $ENV"
+
+if [ ! -z $INTERACTIVE ]
 then
-	INTERACTIVE=""
-	CIDFILE="--cidfile $OUT/container.id"
-	echo "Run docker in NON-interactive mode"
-	shift
-else
-	INTERACTIVE="-it"
-	CIDFILE=""
 	echo "Run docker in interactive mode"
 fi
 
@@ -55,7 +91,7 @@ else
 fi
 
 # Z for setting SELinux label
-exec $SUDO_CMD docker run $INTERACTIVE $CIDFILE --rm \
+exec $SUDO_CMD docker run $ENV $INTERACTIVE $CIDFILE --rm \
   -v $SRC:/home/$(id -nu)/src:Z \
   -v $OUT:/home/$(id -nu)/out:Z \
   kernel-build-container:$COMPILER "$@"
