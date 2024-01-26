55210960375d3330af65720ac8bb7c28ea3e6fc3
Valentin Obst <valentin.obst@fkie.fraunhofer.de>
2024-01-26 11:36:57 +0100

    start_container: run shfmt

--- start_container.sh
+++ start_container.sh
@@ -2,9 +2,10 @@
 
 function print_help {
 	echo "usage: $0 compiler src_dir out_dir [-n] [-h] [-e] [-- cmd with args]"
-	echo "  -n    launch container in non-interactive mode"
 	echo "  -e    space-separated list of environment variables"
 	echo "  -h    print this help"
+	echo "  -n    launch container in non-interactive mode"
+	echo "  -v    enable debug output"
 	echo ""
 	echo "  If cmd is empty, we will start an interactive bash in the container."
 }
@@ -14,8 +15,7 @@ NEED_SUDO=$?
 
 set -eu
 
-if [ $NEED_SUDO -eq 1 ]
-then
+if [ $NEED_SUDO -eq 1 ]; then
 	echo "Hey, we gonna use sudo for running docker"
 	SUDO_CMD="sudo"
 else
@@ -23,8 +23,7 @@ else
 	SUDO_CMD=""
 fi
 
-if [ $# -lt 3 ]
-then
+if [ $# -lt 3 ]; then
 	print_help
 	exit 1
 fi
@@ -46,45 +45,46 @@ ENV=""
 INTERACTIVE="-it"
 
 while [[ $# -gt 0 ]]; do
-  case $1 in
-    -n|--non-interactive)
-      INTERACTIVE=""
-      CIDFILE="--cidfile $OUT/container.id"
-      echo "Run docker in NON-interactive mode"
-      shift
-      ;;
-    -e|--env)
-      for var in $2
-      do
-        ENV="$ENV -e $var"
-      done
-      shift 2
-      ;;
-    -h|--help)
-      print_help
-      exit 0
-      ;;
-    --)
-      shift
-      break
-      ;;
-    -*|--*)
-      echo "Unknown option $1"
-      print_help
-      exit 1
-      ;;
-  esac
+	case $1 in
+	-n | --non-interactive)
+		INTERACTIVE=""
+		CIDFILE="--cidfile $OUT/container.id"
+		echo "Run docker in NON-interactive mode"
+		shift
+		;;
+	-e | --env)
+		for var in $2; do
+			ENV="$ENV -e $var"
+		done
+		shift 2
+		;;
+	-v | --verbose)
+		set -x
+		shift
+		;;
+	-h | --help)
+		print_help
+		exit 0
+		;;
+	--)
+		shift
+		break
+		;;
+	-* | --*)
+		echo "Unknown option $1"
+		print_help
+		exit 1
+		;;
+	esac
 done
 
 echo "Container environment: $ENV"
 
-if [ ! -z $INTERACTIVE ]
-then
+if [ ! -z $INTERACTIVE ]; then
 	echo "Run docker in interactive mode"
 fi
 
-if [ $# -gt 0 ]
-then
+if [ $# -gt 0 ]; then
 	echo -e "Gonna run \"$@\"\n"
 else
 	echo -e "Gonna run interactive bash...\n"
@@ -92,6 +92,6 @@ fi
 
 # Z for setting SELinux label
 exec $SUDO_CMD docker run $ENV $INTERACTIVE $CIDFILE --rm \
-  -v $SRC:/home/$(id -nu)/src:Z \
-  -v $OUT:/home/$(id -nu)/out:Z \
-  kernel-build-container:$COMPILER "$@"
+	-v $SRC:/home/$(id -nu)/src:Z \
+	-v $OUT:/home/$(id -nu)/out:Z \
+	kernel-build-container:$COMPILER "$@"
