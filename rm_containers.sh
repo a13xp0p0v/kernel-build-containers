f39e5cfa47b2eb10d1ac2880aa85fe2c5fd244e2
Alexander Popov <alex.popov@linux.com>
2024-08-25 23:40:23 +0300

    Add the missing support for the new compilers
    This is the second part of the previous commit.

--- rm_containers.sh
+++ rm_containers.sh
@@ -24,9 +24,12 @@ $SUDO_CMD docker rmi kernel-build-container:gcc-10
 $SUDO_CMD docker rmi kernel-build-container:gcc-11
 $SUDO_CMD docker rmi kernel-build-container:gcc-12
 $SUDO_CMD docker rmi kernel-build-container:gcc-13
+$SUDO_CMD docker rmi kernel-build-container:gcc-14
 $SUDO_CMD docker rmi kernel-build-container:clang-12
 $SUDO_CMD docker rmi kernel-build-container:clang-13
 $SUDO_CMD docker rmi kernel-build-container:clang-14
 $SUDO_CMD docker rmi kernel-build-container:clang-15
+$SUDO_CMD docker rmi kernel-build-container:clang-16
+$SUDO_CMD docker rmi kernel-build-container:clang-17
 $SUDO_CMD docker ps -a
 $SUDO_CMD docker image ls
