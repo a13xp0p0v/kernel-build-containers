c986402b27491e70f07e0fe506c8306ff195465a
Alexander Popov <alex.popov@linux.com>
2024-12-31 02:53:19 +0300

    Style fixes in test_containers.sh

--- test_containers.sh
+++ test_containers.sh
@@ -3,21 +3,14 @@
 set -x
 set -e
 
-echo "Tests must be run with and without the docker group"
-
 if groups $USER | grep '\<docker\>'; then
-    echo "Running tests with the docker group"
+    echo "The user running this test is in the docker group"
 else
-    echo "Running tests without the docker group"
+    echo "The user running this test is NOT in the docker group (will need sudo)"
 fi
 
-sleep 3 #sleep to notice the current group
-
-# Clear the state before beginning the test
+echo "Clearing the state before the test..."
 python3 manage_containers.py -r
-rm -f .coverage
-
-echo "Clearing coverage cache"
 python3 -m coverage erase
 
 echo "Testing the help flag..."
@@ -72,7 +65,7 @@ python3 -m coverage run -a --branch manage_containers.py -l
 python3 -m coverage run -a --branch manage_containers.py -r
 python3 -m coverage run -a --branch manage_containers.py -l
 
-echo "Testing removal with running containers..."
+echo "Testing removal when containers are running..."
 python3 -m coverage run -a --branch manage_containers.py -b gcc-12
 if groups $USER | grep '\<docker\>'; then
     docker run -d --rm --name test-running kernel-build-container:gcc-12 tail -f /dev/null
@@ -85,7 +78,7 @@ else
 fi
 python3 -m coverage run -a --branch manage_containers.py -r
 
-echo "Testing building an existing container..."
+echo "Testing building an existing image..."
 python3 -m coverage run -a --branch manage_containers.py -b gcc-10
 python3 -m coverage run -a --branch manage_containers.py -b gcc-10
 python3 -m coverage run -a --branch manage_containers.py -b gcc-12
@@ -93,7 +86,7 @@ python3 -m coverage run -a --branch manage_containers.py -b clang-13 #it's the s
 python3 -m coverage run -a --branch manage_containers.py -b all
 python3 -m coverage run -a --branch manage_containers.py -r
 
-echo "Collecting coverage for error handling"
+echo "Testing the error handling..."
 
 echo "Testing without any options..."
 python3 -m coverage run -a --branch manage_containers.py && exit 1
@@ -114,14 +107,14 @@ python3 -m coverage run -a --branch manage_containers.py -b all -r -l && exit 1
 python3 -m coverage run -a --branch manage_containers.py -b all -r -l -q && exit 1
 
 echo "Testing invalid GCC/Clang version..."
-python3 -m coverage run -a --branch manage_containers.py -b gcc-invalid && exit 1 
-python3 -m coverage run -a --branch manage_containers.py -b clang-invalid && exit 1 
+python3 -m coverage run -a --branch manage_containers.py -b gcc-invalid && exit 1
+python3 -m coverage run -a --branch manage_containers.py -b clang-invalid && exit 1
 
 echo "Testing unknown options..."
-python3 -m coverage run -a --branch manage_containers.py --unknown-flag && exit 1 
-python3 -m coverage run -a --branch manage_containers.py -b gcc-4.9 --unknown-flag && exit 1 
+python3 -m coverage run -a --branch manage_containers.py --unknown-flag && exit 1
+python3 -m coverage run -a --branch manage_containers.py -b gcc-4.9 --unknown-flag && exit 1
 
-echo "Testing containers with missing gcc tags"
+echo "Testing containers with missing GCC tags..."
 python3 -m coverage run -a --branch manage_containers.py -b gcc-12
 if groups $USER | grep '\<docker\>'; then
     docker rmi -f kernel-build-container:gcc-12
@@ -133,6 +126,6 @@ else
     sudo docker rmi -f kernel-build-container:clang-13
 fi
 
-echo "All tests completed. Creating report"
+echo "All tests completed. Creating the coverage report..."
 python3 -m coverage report --omit='/usr/lib/python3/dist-packages/*'
 python3 -m coverage html --omit='/usr/lib/python3/dist-packages/*'
