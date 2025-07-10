f3ea5d3d69130f86c5d580e911e9758ff4592334
Alexander Popov <alex.popov@linux.com>
2025-07-11 00:54:13 +0300

    Update tests for manage_images.py (2)

--- test_image_mgmt.sh
+++ test_image_mgmt.sh
@@ -28,7 +28,6 @@ $DELIMITER
 echo "Testing image building..."
 python3 -m coverage run -a --branch manage_images.py -b gcc-8
 python3 -m coverage run -a --branch manage_images.py -b clang-11
-python3 -m coverage run -a --branch manage_images.py -b gcc-9 gcc-10
 
 $DELIMITER
 echo "Testing quiet image building..."
@@ -42,34 +41,35 @@ python3 -m coverage run -a --branch manage_images.py -l
 $DELIMITER
 echo "Testing image removal..."
 python3 -m coverage run -a --branch manage_images.py -r gcc-12
-python3 -m coverage run -a --branch manage_images.py -r clang-15
-python3 -m coverage run -a --branch manage_images.py -r gcc-9 gcc-10
+python3 -m coverage run -a --branch manage_images.py -r clang-11
 python3 -m coverage run -a --branch manage_images.py -r
 
 $DELIMITER
 echo "Testing building and removing all images..."
 python3 -m coverage run -a --branch manage_images.py -b
 python3 -m coverage run -a --branch manage_images.py -r
+python3 -m coverage run -a --branch manage_images.py -b all
+python3 -m coverage run -a --branch manage_images.py -r all
 
 $DELIMITER
 echo "Testing building an existing image..."
 python3 -m coverage run -a --branch manage_images.py -b gcc-6
-python3 -m coverage run -a --branch manage_images.py -b clang-10
+python3 -m coverage run -a --branch manage_images.py -b clang-7
 python3 -m coverage run -a --branch manage_images.py -r
+
+$DELIMITER
+echo "Testing removing a non-existing image..."
+python3 -m coverage run -a --branch manage_images.py -r gcc-13
 
 $DELIMITER
 echo "Testing image removal when containers are running..."
 python3 -m coverage run -a --branch manage_images.py -b gcc-12
-python3 -m coverage run -a --branch manage_images.py -b gcc-14
 $SUDO_CMD docker run -d --rm --name test-running-1 kernel-build-container:gcc-12 tail -f /dev/null
-$SUDO_CMD docker run -d --rm --name test-running-2 kernel-build-container:gcc-14 tail -f /dev/null
 python3 -m coverage run -a --branch manage_images.py -r gcc-12
+python3 -m coverage run -a --branch manage_images.py -r clang-13
 python3 -m coverage run -a --branch manage_images.py -r
 $SUDO_CMD docker stop test-running-1
 python3 -m coverage run -a --branch manage_images.py -r gcc-12
-python3 -m coverage run -a --branch manage_images.py -r
-$SUDO_CMD docker stop test-running-2
-python3 -m coverage run -a --branch manage_images.py -r
 
 $DELIMITER
 echo "Testing the error handling..."
@@ -85,19 +85,28 @@ python3 -m coverage run -a --branch manage_images.py -r strange-compiler && exit
 python3 -m coverage run -a --branch manage_images.py -strange-option && exit 1
 
 $DELIMITER
-echo "Testing invalid combinations..."
+echo "Testing some invalid combinations..."
 python3 -m coverage run -a --branch manage_images.py -q && exit 1
 python3 -m coverage run -a --branch manage_images.py -q -r && exit 1
+python3 -m coverage run -a --branch manage_images.py -q -r gcc-12 && exit 1
+python3 -m coverage run -a --branch manage_images.py -q -l && exit 1
+python3 -m coverage run -a --branch manage_images.py -q -l -r && exit 1
+python3 -m coverage run -a --branch manage_images.py -q -l -r gcc-12 && exit 1
 python3 -m coverage run -a --branch manage_images.py -l -r && exit 1
 python3 -m coverage run -a --branch manage_images.py -l -r gcc-12 && exit 1
-python3 -m coverage run -a --branch manage_images.py -l -q && exit 1
-python3 -m coverage run -a --branch manage_images.py -l -q -r && exit 1
+python3 -m coverage run -a --branch manage_images.py -b -r && exit 1
 python3 -m coverage run -a --branch manage_images.py -b gcc-10 -r && exit 1
-python3 -m coverage run -a --branch manage_images.py -b gcc-10 -q -r gcc-10 && exit 1
+python3 -m coverage run -a --branch manage_images.py -b -r gcc-12 && exit 1
+python3 -m coverage run -a --branch manage_images.py -b gcc-10 -r gcc-12 && exit 1
+python3 -m coverage run -a --branch manage_images.py -b gcc-10 -r gcc-12 -q && exit 1
 python3 -m coverage run -a --branch manage_images.py -b -l && exit 1
+python3 -m coverage run -a --branch manage_images.py -b gcc-10 -l && exit 1
+python3 -m coverage run -a --branch manage_images.py -b gcc-10 -l -q && exit 1
 python3 -m coverage run -a --branch manage_images.py -b -l -r && exit 1
-python3 -m coverage run -a --branch manage_images.py -b -l -q && exit 1
-python3 -m coverage run -a --branch manage_images.py -b -l -q -r && exit 1
+python3 -m coverage run -a --branch manage_images.py -b gcc-10 -l -r && exit 1
+python3 -m coverage run -a --branch manage_images.py -b -l -r gcc-12 && exit 1
+python3 -m coverage run -a --branch manage_images.py -b gcc-10 -l -r gcc-12 && exit 1
+python3 -m coverage run -a --branch manage_images.py -b gcc-10 -l -r gcc-12 -q && exit 1
 
 $DELIMITER
 echo "Testing containers with missing GCC tags..."
