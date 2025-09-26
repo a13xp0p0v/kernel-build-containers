a64f087b880b0b96acd793f0d804f916df18d7eb
valera disgrace <djdisgrace@protonmail.com>
2025-09-26 14:29:11 +1000

    refactor(finish_container.sh): simplify logic, refine arguments order

--- finish_container.sh
+++ finish_container.sh
@@ -1,47 +1,42 @@
 #!/bin/bash
 
-if [ $# -ne 3 ]; then
-	echo "usage: $0 kill/nokill out_dir podman/docker"
+print_help(){
+	echo "usage: $0 podman/docker kill/nokill out_dir"
+	echo "  docker/podman -- docker or podman container"
 	echo "  kill/nokill -- how to finish: kill the container and then clean up / only clean up"
 	echo "  out_dir -- build output directory used by this container (with container.id file)"
-	echo "  docker/podman -- docker or podman image runtime"
+	exit 1
+}
+
+set -eu
+
+if [ $# -ne 3 ]; then
+	print_help
 	exit 1
 fi
 
-ACTION="$1"
-OUT="$2"
-RUNTIME="$3"
+RUNTIME="$1"
+ACTION="$2"
+OUT="$3"
 SUDO_CMD=""
 CID_FILE="$OUT/container.id"
 
-output=$($RUNTIME ps 2>&1)
-set -eu
-
-if [ "$RUNTIME" = "podman" ]; then
-	if [ "$EUID" -eq 0 ]; then
-		echo "Dont use podman with sudo, it's rootless!" >&2
-		exit 1
-	fi
-	echo "Hey, you are running podman as $USERNAME ($(id -u))"
-elif [ "$RUNTIME" = "docker" ]; then
-	if echo "$output" | grep -qi "CONTAINER ID"; then
-		echo "Enough permissions to run docker, sudo is not needed"
-	elif echo "$output" | grep -qi "permission denied"; then
-		echo "Hey, we gonna use sudo for running docker"
-		SUDO_CMD="sudo"
-	fi
-else
-	echo "unexpected state or image runtime engine" >&2
-	echo "usage: $0 kill/nokill out_dir podman/docker podman/docker"
-	echo "  kill/nokill -- how to finish: kill the container and then clean up / only clean up"
-	echo "  out_dir -- build output directory used by this container (with container.id file)"
-	echo "  docker/podman -- docker or podman image runtime"
+if [ "$RUNTIME" != "podman" ] && [ $RUNTIME != "docker" ]; then
+	print_help
 	exit 1
 fi
 
+set +e
+RUNTIME_TEST_OUTPUT="$($RUNTIME ps 2>&1)"
+set -e
+
+if echo "$RUNTIME_TEST_OUTPUT" | grep -qi "permission denied"; then
+	echo "Hey, we gonna use sudo for running the container"
+	SUDO_CMD="sudo"
+fi
+
 if [ $ACTION != "kill" -a $ACTION != "nokill" ]; then
-	echo "You have to choose: kill or nokill"
-	echo "usage: $0 kill/nokill out_dir podman/docker"
+	echo "usage: $0 podman/docker kill/nokill out_dir"
 	exit 1
 fi
 
